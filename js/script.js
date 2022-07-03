9a10e06d42cc3111ba812895a583816bdabbae94
Ivan <Priest.ivan14@gmail.com>
2022-07-03 21:54:39 +0300

    Добавлен функционал Таймера с его окончанием и т.п.

--- js/script.js
+++ js/script.js
@@ -48,13 +48,6 @@ let tab = document.querySelectorAll('.info-header-tab'),
 
     hideContent();
 
-    function showPhotoSlider()
-    {
-        {
-        photoSlider.classList.remove('hide');
-        photoSlider.classList.add('show');
-        }
-    };
 
         info.addEventListener('click', function(event)
         {
@@ -138,9 +131,66 @@ let tab = document.querySelectorAll('.info-header-tab'),
             }
         });
 
+        let deadline = '2022-07-05';
+
+        function getTimeRemaining(endtime)
+    {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+        
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    };
+
+    function setClock(id, endtime)
+    {
+    let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds'),
+        timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock()
+    {
+        let t = getTimeRemaining(endtime);
+
+        function addZero(num)
+        {
+        if( num<=9 )
+        {
+            return '0' + num;
+        }
+        else return num;
+        };
+
+        hours.textContent = addZero(t.hours);
+        minutes.textContent = addZero(t.minutes);
+        seconds.textContent = addZero(t.seconds);
+
+        if (t.total <= 0)
+        {
+            clearInterval(timeInterval);
+            hours.textContent = '00';
+            minutes.textContent = '00';
+            seconds.textContent = '00';
+        };
+
+    };
+};
+
+setClock('timer', deadline);
+
+
 
 
 
 
 
 });
+
