020b6ce9f99e9d2609852c48713cbd8f42c7f007
Ivan <Priest.ivan14@gmail.com>
2022-08-24 10:32:24 +0300

    Изменен метод отправки данных на сервер php на Promise

--- js/script.js
+++ js/script.js
@@ -229,35 +229,42 @@ let form = document.querySelector('.main-form'),
     input = form.getElementsByTagName('input'),
     statusMessage = document.createElement('div');
 
-statusMessage.classList.add ('status');
-form.addEventListener('submit', function (event){
+    statusMessage.classList.add ('status');
+    form.addEventListener('submit', function (event){
     event.preventDefault();
     form.appendChild(statusMessage);
+    let formData = new FormData(form);
+
+function postData (data) {
+return new Promise (function(resolve,reject){
     let request = new XMLHttpRequest();
     request.open ('POST', 'server.php');
-
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    let formData = new FormData(form);
-    let obj = {};
-    formData.forEach(function(value, key) {
-        obj[key] = value;
-    });
-    let json = JSON.stringify(obj);
-    request.send(json);
-
-    request.addEventListener ('readystatechange', function(){
-        if(request.readyState < 4) {
-            statusMessage.innerHTML = message.loading;
+    request.setRequestHeader ('Content-type', 'application/json; charset=utf-8');
+    request.onreadystatechange = function () {
+        if (request.readyState < 4) {
+            resolve()
         } else if (request.readyState === 4 && request.status == 200) {
-            statusMessage.innerHTML = message.success;
+            resolve()
         } else {
-            statusMessage.innerHTML = message.failure;
+            reject()
         }
-    });
-
-for (let i=0; i<input.length; i++) {
-    input [i].value = '';
+    }
+    request.send (data);
+})
 }
+
+function clearInput(){
+    for( let i=0; i<input.length; i++) {
+        input [i].value = '';
+    }
+}
+
+postData(formData)
+.then ( () => statusMessage.innerHTML = message.loading)
+.then ( () => statusMessage.innerHTML = message.success)
+.catch ( () => statusMessage.innerHTML = message.failure)
+.then (clearInput)
+
 });
 
 
@@ -273,31 +280,38 @@ let formContact = document.querySelector("#form"),
     formContact.addEventListener('submit', function(event){
     event.preventDefault();
     formContact.appendChild(messageContact);
-
-    let request = new XMLHttpRequest();
-    request.open('POST', 'server.php');
-    request.setRequestHeader('content-type', 'application/JSON; charset=utf-8');
     let formData = new FormData(formContact);
-    let obj = {};
-    formData.forEach(function(value,key){
-        obj[key] = value;
-    });
-    let json = JSON.stringify(obj);
-    request.send(json);
 
-    request.addEventListener ('readystatechange', function(){
-        if(request.readyState < 4) {
-            messageContact.innerHTML = message.loading;
-        } else if (request.readyState === 4 && request.status == 200) {
-            messageContact.innerHTML = message.success;
-        } else {
-            messageContact.innerHTML = message.failure;
+    function postData (data) {
+        return new Promise (function(resolve,reject){
+            let request = new XMLHttpRequest();
+            request.open ('POST', 'server.php');
+            request.setRequestHeader ('Content-type', 'application/json; charset=utf-8');
+            request.onreadystatechange = function () {
+                if (request.readyState < 4) {
+                    resolve()
+                } else if (request.readyState === 4 && request.status == 200) {
+                    resolve()
+                } else {
+                    reject()
+                }
+            }
+            request.send (data);
+        })
         }
-    });
 
-for (let i=0; i<inputContact.length; i++) {
-    inputContact [i].value = '';
-}
+        function clearInput(){
+            for( let i=0; i<inputContact.length; i++) {
+                inputContact [i].value = '';
+            }
+        }
+
+        postData(formData)
+        .then ( () => messageContact.innerHTML = message.loading)
+        .then ( () => messageContact.innerHTML = message.success)
+        .catch ( () => messageContact.innerHTML = message.failure)
+        .then (clearInput)
+
 });
 
 
